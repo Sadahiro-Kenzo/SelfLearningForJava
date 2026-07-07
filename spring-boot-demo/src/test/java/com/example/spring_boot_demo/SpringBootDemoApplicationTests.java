3cb3d15b42837b21077c4cac65b46cb0957d2f5d
Sadahiro Kenzo <sadahiro.kenzo+git@gmail.com>
2026-07-07 10:40:23 +0900

    アプリケーションとWebコントローラ分割。単体テスト追加。管理サービス追加

--- spring-boot-demo/src/test/java/com/example/spring_boot_demo/SpringBootDemoApplicationTests.java
+++ spring-boot-demo/src/test/java/com/example/spring_boot_demo/SpringBootDemoApplicationTests.java
@@ -1,13 +1,30 @@
 package com.example.spring_boot_demo;
 
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class SpringBootDemoApplicationTests {
 
+	@Autowired
+	private MockMvc mvc;
+
 	@Test
-	void contextLoads() {
+	void getHello() throws Exception {
+		mvc.perform(get("/").accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk())
+				.andExpect(content().string(equalTo("Greetings from Spring Boot!")));
 	}
 
 }
