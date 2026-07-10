37ab765d04a44b9b1f1b0a0d0cbd53f307b385da
Sadahiro Kenzo <sadahiro.kenzo+git@gmail.com>
2026-07-10 14:34:01 +0900

    Spring Data REST API の自動生成 (JPA)

---
+++ accessing_data_rest/src/test/java/com/example/accessing_boot_rest/AccessingBootRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.accessing_boot_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccessingBootRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
