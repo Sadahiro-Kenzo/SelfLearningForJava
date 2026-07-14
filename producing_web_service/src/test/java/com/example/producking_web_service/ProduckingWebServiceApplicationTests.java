35fc262185d2be2e9161d49adfc8691c98988bba
Sadahiro Kenzo <sadahiro.kenzo+git@gmail.com>
2026-07-14 17:21:17 +0900

    SOAP Web サービスの生成

---
+++ producing_web_service/src/test/java/com/example/producking_web_service/ProduckingWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.producking_web_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProduckingWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
